e145dfd645dca65293f3351f4848a024edbf30f8
Tirth Rojara <work.tirthrojara@gmail.com>
2026-03-15 01:03:50 +0530

    Update: next.js version

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -172,7 +172,7 @@ export default function Skills() {
         </div>
 
         {/* Floating tech badges */}
-        {/* <motion.div
+        <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.6, delay: 0.7 }}
@@ -187,7 +187,7 @@ export default function Skills() {
               {tech}
             </motion.span>
           ))}
-        </motion.div> */}
+        </motion.div>
       </div>
     </section>
   )
